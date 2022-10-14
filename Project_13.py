e5813f0247dbeef28c02868c77b86f419b51dd7b
Ancient_dev <yadavaneek08@gmail.com>
2022-10-15 01:03:38 +0530

    Days in month

--- Project_13.py
+++ Project_13.py
@@ -1,4 +1,4 @@
-def is_leap(year):
+def is_leap_year(year):
   if year % 4 == 0:
     if year % 100 == 0:
       if year % 400 == 0:
@@ -11,7 +11,7 @@ def is_leap(year):
         return False
 def days_in_month(year, month):
   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
-  if is_leap(year) and month == 2:
+  if is_leap_year(year) and month == 2:
       return 29
   else:
       return month_days[month - 1]
