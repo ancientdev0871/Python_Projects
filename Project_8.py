2d4ee50c9507c2fc07cff922ae9c037b1fcce8b4
Aneek <yadavaneek08@gmail.com>
2022-10-15 00:41:18 +0530

    Paint area calculator

--- Project_8.py
+++ Project_8.py
@@ -1,4 +1,4 @@
-def paint_calc(height, width, cover):
+def paint_calci(height, width, cover):
     calc = (test_h * test_w)/5
     calc2 = round(calc)
     print(f"You'll need {calc2} cans of paint.")
@@ -6,7 +6,7 @@ def paint_calc(height, width, cover):
 test_h = int(input("Height of wall: "))
 test_w = int(input("Width of wall: "))
 coverage = 5
-paint_calc(height=test_h, width=test_w, cover=coverage)
+paint_calci(height=test_h, width=test_w, cover=coverage)
 
 
 
